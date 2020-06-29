6f96d1ba2723e48648ee6572e6f80a1f2cc93187
GinLei <1046129657@qq.com>
2020-06-29 20:55:41 +0800

    添加菜单归零选项

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -34,5 +35,11 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText(String.valueOf(--number));
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
